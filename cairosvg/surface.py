078133d88805933c842a1ffddd70029f7f8e4201
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2011-12-14 19:19:05 +0100

    Fix stupid bug

--- cairosvg/surface.py
+++ cairosvg/surface.py
@@ -365,7 +365,7 @@ class Surface(object):
         if join_cap == "bevel":
             self.context.set_line_join(cairo.LINE_JOIN_BEVEL)
 
-        miter_limit = size(node.get("stroke-miterlimit", 4))
+        miter_limit = float(node.get("stroke-miterlimit", 4))
         self.context.set_miter_limit(miter_limit)
 
         if hasattr(self, node.tag):
