fcc0857cc3d35b27e7ac00ede4cc50a56e4edf49
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2014-08-12 09:20:47 +0200

    Add Python 3.4 in list of supported versions

--- setup.py
+++ setup.py
@@ -56,4 +56,5 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Topic :: Multimedia :: Graphics :: Graphics Conversion"])
