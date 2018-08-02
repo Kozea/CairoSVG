a0ad9c1775840224757c65eb372a1c5504327307
aziesemer <aziesemer@users.noreply.github.com>
2018-08-02 16:54:56 -0400

    Still trying to make Travis happy

--- cairosvg/surface.py
+++ cairosvg/surface.py
@@ -140,37 +140,38 @@ class Surface(object):
         """
         trees = []
         if (bytestrings is not None):
-            if not isinstance(bytestrings, list): 
+            if not isinstance(bytestrings, list):
                 bytestrings = [bytestrings]
             for bytestring in bytestrings:
-                trees.append(Tree(bytestring=bytestring, unsafe=unsafe, 
+                trees.append(Tree(bytestring=bytestring, unsafe=unsafe,
                                   **kwargs))
 
         if(file_objs is not None):
-            if not isinstance(file_objs, list): 
+            if not isinstance(file_objs, list):
                 file_objs = [file_objs]
             for file in file_objs:
                 trees.append(Tree(file_obj=file, unsafe=unsafe, **kwargs))
 
         if (urls is not None):
-            if not isinstance(urls, list): 
+            if not isinstance(urls, list):
                 urls = [urls]
             for url in urls:
                 trees.append(Tree(url=url, unsafe=unsafe, **kwargs))
 
         if (tree_objs is not None):
-            if not isinstance(tree_objs, list): 
+            if not isinstance(tree_objs, list):
                 tree_objs = [tree_objs]
             trees.extend(tree_objs)
 
         output = write_to or io.BytesIO()
 
+        instance = None
         for tree in trees:
-            if 'instance' in locals():
-                instance.addPage(tree, parent_width, parent_height, scale)
-            else:
-                instance = cls(tree, output, dpi, None, parent_width, 
+            if instance is None:
+                instance = cls(tree, output, dpi, None, parent_width,
                                parent_height, scale)
+            else:
+                instance.addPage(tree, parent_width, parent_height, scale)
 
         instance.finish()
         if write_to is None:
@@ -498,7 +499,7 @@ class PDFSurface(Surface):
         width *= scale
         height *= scale
         self.cairo.show_page()
-        self.set_context_size(width, height, viewbox, scale, 
+        self.set_context_size(width, height, viewbox, scale,
                               preserved_ratio(tree))
         self.cairo.set_size(width * self.device_units_per_user_units,
                             height * self.device_units_per_user_units)
