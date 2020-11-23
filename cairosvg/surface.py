83c0b83d664e24d1ddb7011ede967103acff2d2d
Guillaume Ayoub <guillaume@courtbouillon.org>
2020-11-23 18:51:03 +0100

    Follow hint when only output_width or output_height is set
    When only one of the two sizes is given, the other one is defined according to
    the intrinsic ratio.
    
    Fix #164.

--- cairosvg/surface.py
+++ cairosvg/surface.py
@@ -184,6 +184,14 @@ class Surface(object):
 
         if output_width and output_height:
             width, height = output_width, output_height
+        elif output_width:
+            if width:
+                height *= output_width / width
+            width = output_width
+        elif output_height:
+            if height:
+                width *= output_height / height
+            height = output_height
         else:
             width *= scale
             height *= scale
